0c8bda3394946ea776349fcd66610c2238c21294
HolyDos <holydos@rambler.ru>
2016-05-11 16:43:29 +0300

    added basic functional

--- src/main/java/vutichenko/play/Playground.java
+++ src/main/java/vutichenko/play/Playground.java
@@ -1,10 +1,17 @@
 package main.java.vutichenko.play;
 
+import main.java.vutichenko.play.OnFaceWriters.ConsoleOnFace;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by vutichenko on 11.05.2016.
  */
 public class Playground {
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("/spring-config.xml");
 
+        ConsoleOnFace consoleWriter = context.getBean(ConsoleOnFace.class);
+        consoleWriter.writeOnFace();
     }
 }
